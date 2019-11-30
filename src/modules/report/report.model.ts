cdfce69a7c79eea1ba123148ce76edadea2779ba
Adam <adam.lj.haglund@gmail.com>
2019-11-30 20:09:23 +0100

    add reports

---
+++ src/modules/report/report.model.ts
@@ -0,0 +1,28 @@
+import { Column, Entity } from 'typeorm'
+
+import { ExtendedEntity } from '@/modules/exented-entity'
+import { OptionalUuid } from '@/utils'
+
+type LikeConstructor = OptionalUuid<
+  Pick<Report, 'uuid' | 'sessionUuid' | 'stratUuid' | 'message'>
+>
+
+@Entity()
+export class Report extends ExtendedEntity {
+  @Column({ type: 'uuid' })
+  sessionUuid: string
+
+  @Column({ type: 'uuid' })
+  stratUuid: string
+
+  @Column()
+  message: string
+
+  constructor(options: LikeConstructor) {
+    super(options)
+
+    this.sessionUuid = options?.sessionUuid
+    this.stratUuid = options?.stratUuid
+    this.message = options?.message
+  }
+}
