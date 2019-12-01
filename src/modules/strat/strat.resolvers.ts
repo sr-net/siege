020b44b571c5ee9629287ff8acf971647e7cb8d2
Adam <adam.lj.haglund@gmail.com>
2019-12-01 15:21:27 +0100

    fix resolvers

--- src/modules/strat/strat.resolvers.ts
+++ src/modules/strat/strat.resolvers.ts
@@ -1,4 +1,4 @@
-import { Arg, ID, Query, Resolver } from 'type-graphql'
+import { Arg, ID, Int, Query, Resolver } from 'type-graphql'
 
 import { Strat } from '@/modules/strat/strat.model'
 
@@ -6,16 +6,21 @@ import { Strat } from '@/modules/strat/strat.model'
 export class StratResolver {
   @Query(() => Strat, { nullable: true })
   public async strat(
-    @Arg('uuid', () => ID, { nullable: true }) uuid: string,
-    @Arg('shortId', () => ID, { nullable: true }) shortId: string,
+    @Arg('uuid', () => ID, { nullable: true })
+    uuid?: string,
+    @Arg('shortId', () => ID, { nullable: true })
+    shortId?: string,
   ): Promise<Strat | null> {
-    return (
-      (await Strat.findOne({
-        where: [
-          { uuid, submission: false },
-          { shortId, submission: false },
-        ],
-      })) ?? null
-    )
+    return (await Strat.findOne({ where: [{ uuid }, { shortId }] })) ?? null
+  }
+
+  @Query(() => [Strat])
+  public async strats(
+    @Arg('page', () => Int, { nullable: true, defaultValue: 0 })
+    page: number,
+  ): Promise<Strat[]> {
+    const strats = await Strat.find({ take: 10, skip: 10 * page })
+
+    return strats
   }
 }
