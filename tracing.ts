98b1b87b4dda5e59367dd37727c0525766f1562f
Adam <adam@haglund.dev>
2022-07-19 21:09:51 +0200

    remove tracing

--- tracing.ts
+++
@@ -1,9 +0,0 @@
-import { lightstep } from "lightstep-opentelemetry-launcher-node"
-
-const sdk = lightstep.configureOpenTelemetry({
-  serviceName: "siege",
-})
-
-if (process.env.OTEL_EXPORTER_OTLP_SPAN_ENDPOINT != null) {
-  sdk.start()
-}
