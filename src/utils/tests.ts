eca3a27eebc13f59d49db33a57557e442456ba87
Adam <adam@haglund.dev>
2022-09-21 18:31:25 +0200

    delete unused file

--- src/utils/tests.ts
+++
@@ -1,6 +0,0 @@
-export const assertObjectEquals = <T extends Record<string, unknown>>(
-  result: T,
-  user: T,
-) => {
-  expect(JSON.stringify(result, null, 2)).toEqual(JSON.stringify(user, null, 2))
-}
