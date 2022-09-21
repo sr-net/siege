d1059123984811ce7fc9e518e559bd4f1366e676
Adam <adam@haglund.dev>
2022-09-21 16:45:21 +0200

    add mutations

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,23 +1,3 @@
-import { idArg, nonNull, objectType } from "nexus"
-
-import { Strat } from "@/modules/strat/strat.graphql"
-
 export { queryStrat, queryStrats } from "@/modules/strat/strat.graphql"
 
-export const Mutation = objectType({
-  name: "Mutation",
-  definition(t) {
-    t.field("likeStrat", {
-      type: Strat,
-      args: {
-        uuid: nonNull(idArg()),
-      },
-    })
-    t.field("unlikeStrat", {
-      type: Strat,
-      args: {
-        uuid: nonNull(idArg()),
-      },
-    })
-  },
-})
+export { mutationLikeStrat, mutationUnlikeStrat } from "@/modules/like/like.graphql"
--- src/modules/like/like.graphql.ts
+++ src/modules/like/like.graphql.ts
@@ -1,7 +1,9 @@
-import { FieldResolver } from "nexus"
+import { FieldResolver, idArg, mutationField, nonNull } from "nexus"
+import { isNil } from "remeda"
 import { dedent } from "ts-dedent"
 
 import { dbClient } from "@/db"
+import { NexusGenTypes } from "@/graphql/types.generated"
 
 const likedQuery = dedent`
   SELECT EXISTS (
@@ -23,3 +25,117 @@ export const resolveLiked: FieldResolver<"Strat", "liked"> = (strat, _, ctx) => 
     sessionId: ctx.sessionUuid,
   })
 }
+
+// Mutations
+
+const likeQuery = dedent`
+  select (
+    insert \`Like\` {
+      active := true,
+      sessionId := <uuid>$sessionId,
+      strat := (
+        select Strat filter .id = <uuid>$stratId
+      )
+    }
+    unless conflict on ((.strat, .sessionId))
+    else (
+      update \`Like\`
+      set {
+        active := true
+      }
+    )
+  ) {
+    strat: {
+      uuid := .id,
+      shortId,
+      title,
+      description,
+      atk,
+      def,
+      gamemodes,
+      score,
+      author: {
+        name,
+        type := .kind,
+        url,
+      },
+    }
+  }
+`
+
+export const mutationLikeStrat = mutationField("likeStrat", {
+  type: "Strat",
+  args: {
+    uuid: nonNull(idArg()),
+  },
+
+  resolve: async (_, args, ctx) => {
+    if (isNil(ctx.sessionUuid)) {
+      ctx.sessionUuid = ctx.setSessionUuid()
+    }
+
+    const result = await dbClient.queryRequiredSingle<{
+      strat: Omit<NexusGenTypes["allTypes"]["Strat"], "liked">
+    }>(likeQuery, {
+      stratId: args.uuid,
+      sessionId: ctx.sessionUuid,
+    })
+
+    ctx.logger.debug({ result })
+
+    return result.strat
+  },
+})
+
+const unlikeQuery = dedent`
+  select (
+    update \`Like\`
+    filter
+      .strat.id = <uuid>$stratId
+    and
+      .sessionId = <uuid>$sessionId
+    set {
+      active := false,
+    }
+  ) {
+    strat: {
+      uuid := .id,
+      shortId,
+      title,
+      description,
+      atk,
+      def,
+      gamemodes,
+      score,
+      author: {
+        name,
+        type := .kind,
+        url,
+      },
+    }
+  }
+`
+
+export const mutationUnlikeStrat = mutationField("unlikeStrat", {
+  type: "Strat",
+  args: {
+    uuid: nonNull(idArg()),
+  },
+
+  resolve: async (_, args, ctx) => {
+    if (isNil(ctx.sessionUuid)) {
+      ctx.sessionUuid = ctx.setSessionUuid()
+    }
+
+    const result = await dbClient.queryRequiredSingle<{
+      strat: Omit<NexusGenTypes["allTypes"]["Strat"], "liked">
+    }>(unlikeQuery, {
+      stratId: args.uuid,
+      sessionId: ctx.sessionUuid,
+    })
+
+    ctx.logger.debug({ result })
+
+    return result.strat
+  },
+})
